0d9c0941edbe426612431531de5bd1535a6850b7
sesael <85567203+sesael@users.noreply.github.com>
2024-08-21 16:27:27 +0000

    solved

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
-from utils import db_connect
-engine = db_connect()
+#from utils import db_connect
+#engine = db_connect()
 
 # your code here
+
+#from utils import db_connect
+#engine = db_connect()
+
